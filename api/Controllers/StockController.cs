0e84cf47330b427f3c2f9fad33c6f622ed5b5ff1
Vladislava Diachenko <v.diachenko@ukma.edu.ua>
2025-04-23 13:34:47 +0100

    CRUD endpoints for Stock entity done with DTOs and mappers as extension methods

--- api/Controllers/StockController.cs
+++ api/Controllers/StockController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using api.Data;
+using api.DTOs.Stock;
 using api.Mappers;
 using Microsoft.AspNetCore.Mvc;
 
@@ -29,7 +30,7 @@ namespace api.Controllers
 
         [HttpGet]
         [Route("{id}")]
-        public IActionResult GetStockById(int id)
+        public IActionResult GetStockById([FromRoute] int id)
         {
             var stock = _db.Stocks.FirstOrDefault(u => u.StockId == id);
             if (stock == null)
@@ -37,6 +38,54 @@ namespace api.Controllers
                 return NotFound();
             }
             return Ok(stock.ToStockDto());
+        }
+
+        [HttpPost]
+        public IActionResult CreateStock([FromBody] CreateStockRequestDTO stockDTO)
+        {
+            var stockModel = stockDTO.ToStockFromCreateDto();
+
+            _db.Stocks.Add(stockModel);
+            _db.SaveChanges();
+            return CreatedAtAction(nameof(GetStockById), new { id = stockModel.StockId }, stockModel.ToStockDto());
+        }
+
+        [HttpPut]
+        [Route("{id}")]
+        public IActionResult UpdateStock([FromRoute] int id, [FromBody] UpdateStockRequestDTO updateDTO)
+        {
+            var stockModel = _db.Stocks.FirstOrDefault(u => u.StockId == id);
+            if (stockModel == null)
+            {
+                return NotFound();
+            }
+
+            stockModel.Symbol = updateDTO.Symbol;
+            stockModel.CompanyName = updateDTO.CompanyName;
+            stockModel.Purchase = updateDTO.Purchase;
+            stockModel.LastDiv = updateDTO.LastDiv;
+            stockModel.Industry = updateDTO.Industry;
+            stockModel.MarketCap = updateDTO.MarketCap;
+
+            _db.SaveChanges();
+
+            return Ok(stockModel.ToStockDto());
+        }
+
+        [HttpDelete]
+        [Route("{id}")]
+        public IActionResult DeleteStock([FromRoute] int id)
+        {
+            var stock = _db.Stocks.FirstOrDefault(u => u.StockId == id);
+            if (stock == null)
+            {
+                return NotFound();
+            }
+
+            _db.Stocks.Remove(stock);
+            _db.SaveChanges();
+
+            return Ok(stock);
         }
     }
 }
